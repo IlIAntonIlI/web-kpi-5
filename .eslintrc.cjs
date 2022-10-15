8916f1571308f3bb1a41aa64a3e75ce003e0f086
IlIAntonIlI <anton.adamchuk.kpi@gmail.com>
2022-10-15 17:33:13 +0300

    lab1. Added prettier config and integrated with eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,14 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true
-  },
-  extends: ['standard', 'plugin:sonar/recommended'],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module'
-  },
-  plugins: ['sonar'],
-  rules: {
-  }
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: ['standard', 'plugin:sonar/recommended', 'eslint-config-prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+    },
+    plugins: ['sonar'],
+    rules: {},
 }
