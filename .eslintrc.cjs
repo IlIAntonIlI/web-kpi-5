d75af93f47e5854d5e2635120eb58f484c2ad073
IlIAntonIlI <anton.adamchuk.kpi@gmail.com>
2022-10-15 17:21:26 +0300

    lab1. Initialized eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    es6: true
+  },
+  extends: 'standard',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
