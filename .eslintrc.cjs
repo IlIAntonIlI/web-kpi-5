8297effa973b9894839e135e862b5343629f6fe3
IlIAntonIlI <anton.adamchuk.kpi@gmail.com>
2022-10-15 17:25:22 +0300

    lab1. Added sonar

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true,
-    es6: true
+    node: true
   },
-  extends: 'standard',
+  extends: ['standard', 'plugin:sonar/recommended'],
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module'
   },
+  plugins: ['sonar'],
   rules: {
   }
 }
