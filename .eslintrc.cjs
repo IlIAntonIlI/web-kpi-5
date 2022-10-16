e86bd25e03d88c968b0b9fbb8250b64ba9e59c84
IlIAntonIlI <anton.adamchuk.kpi@gmail.com>
2022-10-16 13:57:03 +0300

    lab1. Added plugin eslint n integration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,18 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    extends: ['standard', 'plugin:sonar/recommended', 'eslint-config-prettier'],
+    extends: [
+        'standard',
+        'plugin:sonar/recommended',
+        'plugin:n/recommended',
+        'eslint-config-prettier',
+    ],
     overrides: [],
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['sonar'],
+    plugins: ['sonar', 'n'],
     globals: {
         Response: 'readonly',
     },
