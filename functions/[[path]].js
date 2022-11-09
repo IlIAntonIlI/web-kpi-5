8ea1c272557ae40d5eca873ad20cc670b0fc6496
IlIAntonIlI <anton.adamchuk.kpi@gmail.com>
2022-11-09 23:37:16 +0200

    lab2. Added handle method and created routes for GET and POST

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,3 +1,16 @@
-export async function onRequest() {
-    return new Response('Hello, world!')
+import { Router } from '../lib/router'
+
+const router = new Router()
+
+router.get('/', (req, res) => {
+    return new Response(JSON.stringify({message: 'Get'}))
+})
+
+router.post('/', (req, res) => {
+    return new Response(JSON.stringify({message: 'Post'}))
+})
+
+export async function onRequest(context) {
+    const { request } = context
+    return router.handle(request, new Response())
 }
